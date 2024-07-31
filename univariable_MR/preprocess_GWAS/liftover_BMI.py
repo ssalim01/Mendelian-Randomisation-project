deb4e91fd8143b55554a4360820552e31abe7bdb
Sanjeedah Salim <128813396+ssalim01@users.noreply.github.com>
2024-07-31 18:50:13 +0100

    Create liftover_BMI.py

---
+++ univariable_MR/preprocess_GWAS/liftover_BMI.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pyliftover import LiftOver
+
+# Initialize the LiftOver object for GRCh37 to GRCh38 conversion
+lo = LiftOver('hg19', 'hg38')
+
+# Load your GWAS data
+input_file = '/Users/sanjeedahs/Desktop/NEWMR/BMI/BMI.tsv'
+output_file = '/Users/sanjeedahs/Desktop/NEWMR/BMI/BMI_38.tsv'
+
+gwas_data = pd.read_csv(input_file, sep='\t')
+
+# Function to perform the liftover and handle missing conversions
+def lift_over_coordinates(chromosome, bp_loc):
+    lifted = lo.convert_coordinate('chr' + str(chromosome), bp_loc)
+    if lifted:
+        return lifted[0][1]
+    else:
+        return None
+
+# Apply the liftover to the bp_loc column and replace the original column
+gwas_data['bp_loc'] = gwas_data.apply(lambda row: lift_over_coordinates(row['chr'], row['bp_loc']), axis=1)
+
+# Save the updated GWAS data to a new file
+gwas_data.to_csv(output_file, sep='\t', index=False)
+
+print(f"Lifted over GWAS data saved to {output_file}")
